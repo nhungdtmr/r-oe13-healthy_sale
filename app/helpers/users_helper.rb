b30ee8c5e61e4e6b01cd608c86503e9f09d527bc
nhungdtmr <nhungdtmr@gmail.com>
2019-01-09 14:30:58 +0700

    Third commit on login-logout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,4 @@
 module UsersHelper
-  def sign_up user
-    session[:user_id] = user.id  	
-  end
-
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by id: session[:user_id]
-    end
-  end
-
-  def logged_in?
-    current_user.present?
-  end
-
   def start_year
     return Date.today.year - Settings.value.start_date
   end
